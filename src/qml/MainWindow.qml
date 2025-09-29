52f8b874b1f45f6a6c555ea0aa40fb0662aac2b1
Olivier Serve <tifauv@gmail.com>
2025-09-30 01:06:39 +0200

    Add a first template for the QML UI

---
+++ src/qml/MainWindow.qml
@@ -0,0 +1,66 @@
+import QtQuick
+import QtQuick.Layouts
+import QtQuick.Controls as Controls
+import org.kde.kirigami as Kirigami
+
+/**
+ * Main window of the Life application.
+ */
+Kirigami.ApplicationWindow {
+    id: root
+
+    title: i18nc("@title:window", "Life")
+
+    width: 600
+    height: 800
+
+    pageStack.initialPage: Kirigami.Page {
+
+        ColumnLayout {
+            RowLayout {
+                id: actionsLayout
+
+                Controls.Button {
+                    id: stepBtn
+                    text: " >| "
+                }
+
+                Controls.Button {
+                    id: playBtn
+                    text: " > "
+                }
+
+                Controls.Slider {
+                    id: speedSld
+                }
+            }
+
+            Item {
+                id: worldView
+            }
+
+            RowLayout {
+                id: controlsLayout
+
+                Controls.Label {
+                    text: "Zoom: "
+                }
+
+                Controls.Slider {
+                    id: zoomSld
+                }
+
+                Controls.Label {
+                    id: zoomLbl
+                    text: "x1"
+                }
+            }
+        }
+        Controls.Label {
+            // Center label horizontally and vertically within parent object
+            anchors.centerIn: parent
+            text: i18n("Hello World!")
+        }
+    }
+
+}
---
+++ src/qml/main.cpp
@@ -0,0 +1,75 @@
+#include <memory>
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQuickStyle>
+#include <KIconTheme>
+#include <KLocalizedString>
+#include <KLocalizedContext>
+
+#include "World.hpp"
+#include "PatternsLibrary.hpp"
+#include "MapReduceEngine.hpp"
+#include "StandardCellRules.hpp"
+
+
+int main(int p_argc, char* p_argv[]) {
+	KIconTheme::initTheme();
+	QApplication app(p_argc, p_argv);
+	KLocalizedString::setApplicationDomain("life");
+	QApplication::setOrganizationName(QStringLiteral("CatWitch"));
+	QApplication::setOrganizationDomain(QStringLiteral("catwitch.eu"));
+	QApplication::setApplicationName(QStringLiteral("Life"));
+	QApplication::setDesktopFileName(QStringLiteral("eu.catwitch.life"));
+
+	// Set the graphical style
+	QApplication::setStyle(QStringLiteral("breeze"));
+	if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
+		QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
+	}
+
+	QQmlApplicationEngine engine;
+
+	engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
+	engine.loadFromModule("eu.catwitch.life", "MainWindow");
+
+	// Fail if the main window was not found
+	if (engine.rootObjects().isEmpty()) {
+		return -1;
+	}
+
+	// Alright, let's go !
+	return app.exec();
+}
+
+#ifdef OLDCODE
+void old_main(int p_argc, char* p_argv[]) {
+	QApplication app(p_argc, p_argv);
+
+	// Create the world
+	std::shared_ptr<World> world = std::make_shared<World>(512, 512);
+	world->init();
+	for (uint i=2; i<world->width()-5; i+=8)
+		PatternsLibrary::drawGliderAt(world, i, 2);
+	world->swap();
+
+	// Create the engine
+	StandardCellRules rules;
+	MapReduceEngine engine(world, rules);
+	engine.setTileSize(32);
+
+	// Create the main window & layout
+	MainWindow mainWindow(world);
+
+	QObject::connect(&mainWindow, &MainWindow::step,
+					 &engine,     &Engine::run);
+	QObject::connect(&engine,     &Engine::cellsChanged,
+					&mainWindow,  &MainWindow::detectStop);
+	QObject::connect(&engine,     &Engine::finished,
+					 &mainWindow, &MainWindow::stepFinished);
+
+	mainWindow.show();
+
+	return app.exec();
+}
+#endif
