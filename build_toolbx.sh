ecdb6cd93d0250c54d272cf2f32a63429b9827e7
Olivier Serve <tifauv@gmail.com>
2025-09-30 01:06:04 +0200

    Fix the Toolbx build script

--- build_toolbx.sh
+++ build_toolbx.sh
@@ -1,6 +1,18 @@
-toolbox create cmake_fc42
-toolbox enter cmake_fc42
-sudo dnf update
-sudo dnf install cmake
-sudo dnf install qt6-qtbase-devel
+#!/bin/bash
+
+NAME=dev-life-fc42
+PKGS="cmake
+    extra-cmake-modules
+    qt6-qtbase-devel
+    qt6-qtdeclarative-devel
+    qt6-qtquickcontrols2-devel
+    kf6-kirigami2-devel
+    kf6-ki18n-devel
+    kf6-kcoreaddons-devel
+    kf6-kiconthemes-devel
+    kf6-qqc2-desktop-style"
+
+toolbox create "${NAME}"
+toolbox run -c "${NAME}" sudo dnf update
+toolbox run -c "${NAME}" sudo dnf install ${PKGS}
 
