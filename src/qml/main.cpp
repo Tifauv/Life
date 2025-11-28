58d45d926ac64ceaed6e115bd9066d09001cda54
Olivier Serve Séchet <tifauv@gmail.com>
2025-11-28 18:27:12 +0100

    qml: setup the Engine like in the QtWidgets version

--- src/qml/main.cpp
+++ src/qml/main.cpp
@@ -29,48 +29,32 @@ int main(int p_argc, char* p_argv[]) {
 		QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
 	}
 
-	QQmlApplicationEngine engine;
+	// Create the world
+	std::shared_ptr<World> world = std::make_shared<World>(512, 512);
+	world->init();
+	for (int i=2; i<world->width()-5; i+=8)
+		PatternsLibrary::drawGliderAt(world, i, 2);
+	world->swap();
 
-	engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
-	engine.loadFromModule("eu.catwitch.life.qml", "MainWindow");
+	// Create the engine
+	StandardCellRules rules;
+	MapReduceEngine* engine = new MapReduceEngine;
+	engine->setWorld(world.get());
+	engine->setCellRules(&rules);
+	engine->setTileSize(32);
+	engine->init();
+
+	QQmlApplicationEngine qmlEngine;
+
+	qmlEngine.rootContext()->setContextObject(new KLocalizedContext(&qmlEngine));
+	qmlEngine.rootContext()->setContextProperty(QStringLiteral("engine"), engine);
+	qmlEngine.loadFromModule("eu.catwitch.life.qml", "MainWindow");
 
 	// Fail if the main window was not found
-	if (engine.rootObjects().isEmpty()) {
+	if (qmlEngine.rootObjects().isEmpty()) {
 		return -1;
 	}
 
 	// Alright, let's go !
 	return app.exec();
 }
-
-#ifdef OLDCODE
-void old_main(int p_argc, char* p_argv[]) {
-	QApplication app(p_argc, p_argv);
-
-	// Create the world
-	std::shared_ptr<World> world = std::make_shared<World>(512, 512);
-	world->init();
-	for (uint i=2; i<world->width()-5; i+=8)
-		PatternsLibrary::drawGliderAt(world, i, 2);
-	world->swap();
-
-	// Create the engine
-	StandardCellRules rules;
-	MapReduceEngine engine(world, rules);
-	engine.setTileSize(32);
-
-	// Create the main window & layout
-	MainWindow mainWindow(world);
-
-	QObject::connect(&mainWindow, &MainWindow::step,
-					 &engine,     &Engine::run);
-	QObject::connect(&engine,     &Engine::cellsChanged,
-					&mainWindow,  &MainWindow::detectStop);
-	QObject::connect(&engine,     &Engine::finished,
-					 &mainWindow, &MainWindow::stepFinished);
-
-	mainWindow.show();
-
-	return app.exec();
-}
-#endif
